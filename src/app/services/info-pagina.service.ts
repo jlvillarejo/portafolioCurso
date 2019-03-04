219a00d5dc85bc1c451d2e7bed481b1e696276d2
Jose Luis Villarejo <jlvillarejo@fsc.ccoo.es>
2019-03-04 14:35:57 +0100

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/info-equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +12,35 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: InfoEquipo[] = [];
+
   constructor( private http: HttpClient ) { 
+    
+    this.cargarInfo();
+    this.cargarEquipo();
 
-    //console.log('Servicio de InfoPagina listo');
+  }
+  
+  private cargarInfo() {
 
-    // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
-
+  
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
-
-      })
-    
+        //console.log(resp);
+  
+      });
   }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-b51e3.firebaseio.com/equipo.json')
+      .subscribe( ( resp: InfoEquipo ) => {
+        this.equipo = resp;
+        console.log(this.equipo);
+      })
+
+  }
+
 }
